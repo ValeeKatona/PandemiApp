8a7a3438f274a1f63f764e4aa9cc4d3e9420f3bf
Katona Valentin <djvalee.yt@gmail.com>
2021-11-19 20:03:15 +0100

    navigation, login error messages

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { AppRoutingModule } from 'src/app/app-routing.module';
@@ -25,11 +25,13 @@ describe('LoaderPage', () => {
     fixture.detectChanges();
   }));
 
-  it('should go to login page after load', () => {
+  it('should go to login page after load', fakeAsync(() => {
     spyOn(router, 'navigate');
 
     component.ngOnInit();
 
-    expect(router.navigate).toHaveBeenCalledWith(['login'])
-});
+    tick(1500);
+
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+}))
 });
